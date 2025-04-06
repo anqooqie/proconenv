aff1a62cc73a522cc70ddfde4a3f325481e4bf12
anqooqie <zzzfuku1991@gmail.com>
2025-04-06 13:59:39 +0900

    Add proconlib_py

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,5 +5,10 @@ if [ ! -d /workspaces/proconenv/lib ]; then
   git -C /workspaces/proconenv/lib submodule update --init --recursive
   git -C /workspaces/proconenv/lib remote set-url origin git@github.com:anqooqie/proconlib.git
 fi
+if [ ! -d /workspaces/proconenv/lib_py ]; then
+  git clone https://github.com/anqooqie/proconlib_py.git /workspaces/proconenv/lib_py
+  git -C /workspaces/proconenv/lib_py submodule update --init --recursive
+  git -C /workspaces/proconenv/lib_py remote set-url origin git@github.com:anqooqie/proconlib_py.git
+fi
 ln -s /workspaces/proconenv/lib/template ~/.config/atcoder-cli-nodejs/cpp
 ln -s /workspaces/proconenv/lib_py/template ~/.config/atcoder-cli-nodejs/py
